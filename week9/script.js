f094e002e72f55d4a2b393796d61521b60de8ab2
OgunniyiOwamamwen <owamamwen@outlook.com>
2022-11-14 07:37:58 +0100

    strtch goals complete

--- week9/script.js
+++ week9/script.js
@@ -1,6 +1,9 @@
 const containers = document.querySelectorAll('.drag_conttainer');
 const draggables = document.querySelectorAll('.key');
 
+let integer = 0;
+let integerTotal = [];
+
 onkeydown = (e) => {
     const keyColor = document.querySelector(`div[data-key="${e.keyCode}"]`);
     const color = keyColor.style.backgroundColor = "Red";
@@ -14,6 +17,16 @@ onkeyup = (e) => {
     const keyColor = document.querySelector(`div[data-key="${e.keyCode}"]`);
     const color = keyColor.style.backgroundColor = "yellow";
     const marginTop = keyColor.style.marginTop ="40px";
+    if(color == 'yellow'){ 
+        integer += 1;
+        integerTotal.push(integer);
+        console.log('Count is: '+ integerTotal) // result 'After 10 presses of a key, reset the position of it's button.'
+    }
+    // reset button if key press is equal 10
+    if(integerTotal.pop() == 10)
+    {
+        window.location.reload();
+    }
 }
 
 
