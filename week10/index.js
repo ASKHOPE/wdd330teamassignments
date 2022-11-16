9c8a873bf1abf27dfc983f60e1ff90d7cd3f3747
ASKHOPE <96995931+ASKHOPE@users.noreply.github.com>
2022-11-17 02:59:56 +0530

    Initial Commit 2 incomplete

--- week10/index.js
+++ week10/index.js
@@ -1,14 +1,42 @@
-import { getJSON, getLocation } from "utilities.js";
+import { getJSON, getLocation } from "./utilities.js";
 const baseUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
 
   function testGetQuakesForLocation() {
   // call the getLocation function to get the lat/long
-  const getLocation = (getLocationFn);
+  const getLoca = (getLocation);
   // use that information to build out the correct URL
  // const geoUrl = baseUrl + // add location information here
   // use the url to request the correct quakes 
-  const geoUrl = baseUrl + getLocation// add location information here
-
+  const geoUrl = baseUrl + getLoca // add location information here
+    // console.log(getLoca);
   //log out the quakes for now.
 }
-getQuakesForLocation();
+
+testGetQuakesForLocation();
+//getQuakesForLocation();
+
+// console.log("geoUrl");    
+
+
+
+//TEST CODE FOR LOCATION REQUEST
+// const options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
+
+// function success(pos) {
+//   const crd = pos.coords;
+
+//   console.log("Your current position is:");
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude: ${crd.longitude}`);
+//   console.log(`More or less ${crd.accuracy} meters.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error, options);
--- week10/utilities.js
+++ week10/utilities.js
@@ -1,12 +1,13 @@
-const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
-
+// const eurl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
+const eurl =  "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-03-02&latitude=43.814540699999995&longitude=-111.78491029999999&maxradiuskm=100";
 export function getJSON(url) {
   return fetch(url)
     .then(function (response) {
       if (!response.ok) {
         throw Error(response.statusText);
       } else {
-        return response.json();
+        return response.json()
+        console.log(JSON.stringify(response.json))
       }
     })
     .catch(function (error) {
@@ -18,4 +19,5 @@ export const getLocation = function (options) {
     navigator.geolocation.getCurrentPosition(resolve, reject, options);
   });
 };
-    
+// console.log(getJSON(navigator.geolocation);
+console.log(getJSON(eurl))
