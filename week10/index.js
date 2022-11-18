feaa5078282ce980ff54a03b592eba6367450969
OgunniyiOwamamwen <owamamwen@outlook.com>
2022-11-18 08:03:27 +0100

    week ten completed

--- week10/index.js
+++ week10/index.js
@@ -1,42 +1,4 @@
-import { getJSON, getLocation } from "./utilities.js";
-const baseUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
+import QuakesController from './QuakesController.js';
 
-  function testGetQuakesForLocation() {
-  // call the getLocation function to get the lat/long
-  const getLoca = (getLocation);
-  // use that information to build out the correct URL
- // const geoUrl = baseUrl + // add location information here
-  // use the url to request the correct quakes 
-  const geoUrl = baseUrl + getLoca // add location information here
-    // console.log(getLoca);
-  //log out the quakes for now.
-}
-
-testGetQuakesForLocation();
-//getQuakesForLocation();
-
-// console.log("geoUrl");    
-
-
-
-//TEST CODE FOR LOCATION REQUEST
-// const options = {
-//   enableHighAccuracy: true,
-//   timeout: 5000,
-//   maximumAge: 0,
-// };
-
-// function success(pos) {
-//   const crd = pos.coords;
-
-//   console.log("Your current position is:");
-//   console.log(`Latitude : ${crd.latitude}`);
-//   console.log(`Longitude: ${crd.longitude}`);
-//   console.log(`More or less ${crd.accuracy} meters.`);
-// }
-
-// function error(err) {
-//   console.warn(`ERROR(${err.code}): ${err.message}`);
-// }
-
-// navigator.geolocation.getCurrentPosition(success, error, options);
+const myQuakesController = new QuakesController('#quakeList');
+myQuakesController.init();
--- week10/utilities.js
+++ week10/utilities.js
@@ -1,23 +1,19 @@
-// const eurl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02";
-const eurl =  "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-03-02&latitude=43.814540699999995&longitude=-111.78491029999999&maxradiuskm=100";
 export function getJSON(url) {
   return fetch(url)
-    .then(function (response) {
+    .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
       } else {
-        return response.json()
-        console.log(JSON.stringify(response.json))
+        //console.log(response.json());
+        return response.json();
       }
     })
-    .catch(function (error) {
+    .catch(function(error) {
       console.log(error);
     });
 }
-export const getLocation = function (options) {
-  return new Promise(function (resolve, reject) {
+export const getLocation = function(options) {
+  return new Promise(function(resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject, options);
   });
 };
-// console.log(getJSON(navigator.geolocation);
-console.log(getJSON(eurl))
