8301eabed2ea298ed1c9686d904f47ad07ac1a61
owamamwen1 <90800458+owamamwen1@users.noreply.github.com>
2022-12-02 15:48:45 +0100

    Update response.js
    message

--- week12/response.js
+++ week12/response.js
@@ -4,7 +4,6 @@ const responseObj = {
   fine: "That sound good",
   "please": "What's Up",
   "please I need help": "How can we help you?",
-  "please": "What's Up",
   "today time": new Date().toDateString(),
   "today date": new Date().toLocaleTimeString(),
   "what year": new Date().getFullYear(),
